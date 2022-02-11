4de5b88d81d95be307cea7163e78a81c3a583d1f
星外之神 <wszqkzqk@qq.com>
2022-02-11 20:47:37 +0800

    Updated by update.sh

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 cd `dirname $0`; pwd
 git add *
 git add .gitignore
-git pull git@github.com:wszqkzqk/archpkgcloudbuild.git
+git pull
 git commit -m 'Updated by update.sh'
-git push -u git@github.com:wszqkzqk/archpkgcloudbuild.git
+git push
