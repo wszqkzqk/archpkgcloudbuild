c79ece2dedfb05377aeb262f485ae45b60cf8452
星外之神 <wszqkzqk@qq.com>
2022-02-12 14:41:00 +0800

    移除Archlinuxcn

--- .ci-aur/entrypoint.sh
+++ .ci-aur/entrypoint.sh
@@ -6,13 +6,13 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-cat << EOM >> /etc/pacman.conf
-[archlinuxcn]
-Server = https://repo.archlinuxcn.org/x86_64
-EOM
+#cat << EOM >> /etc/pacman.conf
+#[archlinuxcn]
+#Server = https://repo.archlinuxcn.org/x86_64
+#EOM
 
 pacman-key --init
-pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
+#pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
 pacman -S --noconfirm yay
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
