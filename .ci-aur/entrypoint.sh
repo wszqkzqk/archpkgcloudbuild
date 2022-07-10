c92839945b8262057a91e93e085a73e7b81f8e44
星外之神 <wszqkzqk@qq.com>
2022-07-10 16:11:50 +0800

    u

--- .ci-aur/entrypoint.sh
+++ .ci-aur/entrypoint.sh
@@ -6,22 +6,26 @@ useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chmod -R a+rw .
 
-#cat << EOM >> /etc/pacman.conf
-#[archlinuxcn]
-#Server = https://repo.archlinuxcn.org/x86_64
-#EOM
+cat << EOM >> /etc/pacman.conf
+[archlinuxcn]
+Server = https://repo.archlinuxcn.org/x86_64
+EOM
 
 pacman-key --init
-pacman -Sy --noconfirm #&& pacman -S --noconfirm archlinuxcn-keyring
-#pacman -S --noconfirm yay
+pacman -Sy --noconfirm && pacman -S --noconfirm archlinuxcn-keyring
+pacman -S --noconfirm yay
+
+pacman -R archlinuxcn-keyring --noconfirm
+sed -i "/[archlinuxcn]/Q" /etc/pacman.conf
+pacman -Syu --noconfirm
 
 # Install yay
-pacman -S --noconfirm --needed git
-git clone https://aur.archlinux.org/yay.git /tmp/yay
-pushd /tmp/yay
-chmod -R a+rw .
-sudo -H -u builder makepkg --skippgpcheck --syncdeps --install --noconfirm
-popd
+#pacman -S --noconfirm --needed git
+#git clone https://aur.archlinux.org/yay.git /tmp/yay
+#pushd /tmp/yay
+#chmod -R a+rw .
+#sudo -H -u builder makepkg --skippgpcheck --syncdeps --install --noconfirm
+#popd
 
 
 sudo --set-home -u builder yay -S --noconfirm --builddir=./ "$pkgname"
